468cf8ab74d235cecfe763ecb58c65a2498944f9
Ivan Eltsov <xpleesid@gmail.com>
2022-07-15 04:02:19 +0400

    fix: remove link from password recovery page

--- src/Pages/RecoverPassword/RecoverPassword.tsx
+++ src/Pages/RecoverPassword/RecoverPassword.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
-import style from './RecoverPassword.module.css';
+
 import { RecoverPasswordForm } from './RecoverPasswordForm/RecoverPasswordForm';
 
 export const RecoverPasswordPage = () => {
