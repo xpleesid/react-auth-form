f23053d30a4d7d871a29d70eae19239833bd08cb
Ivan Eltsov <xpleesid@gmail.com>
2022-07-15 11:57:17 +0400

    fix: set corresponding page title

--- esbuild.config.js
+++ esbuild.config.js
@@ -39,7 +39,7 @@ const plugins = [
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>React + Typescript test app</title>
+    <title>react-auth-form</title>
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap" rel="stylesheet">
